1c338ebc872a8df92abfb3a77dbc5bebd2329ac0
The Brahmnic Boy <thebrahmnicboy@gmail.com>
2023-01-22 01:34:10 +0530

    Finish Adding Questions for Test
    Co-authored-by: Shreyashka  Patel <ShreyashkaPatel@users.noreply.github.com>
    Co-authored-by: Shantanu Parameswaran <shantanup06@users.noreply.github.com>

--- client/src/components/CustomSwitcher.js
+++ client/src/components/CustomSwitcher.js
@@ -5,7 +5,7 @@ import { styled } from '@mui/material/styles';
 import { richBlack, light, medium, dark, deepDark } from './colors';
 
 export const CustomSwitcherGroup = styled(ToggleButtonGroup)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === 'light' ? 'whitesmoke' : light,
+    backgroundColor: theme.palette.mode === 'light' ? deepDark : light,
     borderRadius: '50px',
     '& .MuiToggleButtonGroup-grouped': {
         margin: 0,
@@ -15,11 +15,11 @@ export const CustomSwitcherGroup = styled(ToggleButtonGroup)(({ theme }) => ({
         width: '150px',
         '&:not(:first-of-type)': {
             borderRadius: '50px',
-            border: `6px solid ${theme.palette.mode === 'light' ? 'whitesmoke' : light}`,
+            border: `6px solid ${theme.palette.mode === 'light' ? deepDark : light}`,
         },
         '&:first-of-type': {
             borderRadius: '50px',
-            border: `6px solid ${theme.palette.mode === 'light' ? 'whitesmoke' : light}`,
+            border: `6px solid ${theme.palette.mode === 'light' ? deepDark : light}`,
         },
     },
     '& .MuiToggleButton-root': {
@@ -30,7 +30,7 @@ export const CustomSwitcherGroup = styled(ToggleButtonGroup)(({ theme }) => ({
         font: '600 0.8rem Poppins, sans-serif',
 
         '&:hover': {
-            backgroundColor: theme.palette.mode === 'light' ? light : dark,
+            backgroundColor: theme.palette.mode === 'light' ? medium : dark,
             color: 'black',
             transition: 'all 0.2s ease-in-out',
         },
