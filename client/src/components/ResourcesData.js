9c010af3c9c285b2da6bcacdaf8b615c12d261fd
Manas <manastelavane999@gmail.com>
2023-01-22 12:05:51 +0530

    resource section done

---
+++ client/src/components/ResourcesData.js
@@ -0,0 +1,48 @@
+export const data=[
+    
+    
+    {
+        title: 'Talkspace',
+        description:
+        'Online therapy app connecting users to licensed therapists. Convenient and confidential, available on iOS & Android.',
+        cover: 'talkspace.png',
+        link:'https://www.talkspace.com/'
+    },
+    {
+        title: 'HeadSpace',
+        description:
+            'A mindfulness and meditation app that offers guided meditations and other resources to help users manage stress, anxiety, and sleep',
+        cover: 'headspace.png',
+        link:'https://www.headspace.com/'
+    },
+        {
+            title: 'Sanvello',
+            description:
+                'An app that offers tools for coping with anxiety and depression, such as mood tracking, guided meditations, and CBT-based exercises',
+            cover: 'sanvello.png',
+            link:'https://www.sanvello.com/'
+        },
+        {
+            title: 'Happify',
+            description:
+                'An app that offers evidence-based activities and games to help users improve their mood and reduce stress.',
+            cover: 'happify.png',
+            link:'https://www.happify.com/'
+        },
+        {
+            title: 'Calm',
+            description:
+                'An app that provides tools and resources for stress and anxiety management.',
+            cover: 'Calm.png',
+            link:'https://www.calm.com/'
+        },
+        {
+            title: '7 cups',
+            description:
+                'A mental health support app that connects users with trained listeners and therapists for anonymous, one-on-one support',
+            cover: '7cups.png',
+            link:'https://www.7cups.com/'
+        },
+        
+    
+]
